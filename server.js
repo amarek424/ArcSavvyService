71d3b292db36b59a33ae2f234827c337c8b61579
Brian Dorsey <bdo528@gmail.com>
2018-08-28 22:25:45 -0400

    fixed cors config for auth

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.use(passport.initialize());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, POST, GET, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Max-Age", "3600");
   next();
 });
 
