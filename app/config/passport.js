ce6c2882bbad078d97164a87ea5c47ce80feed99
Brian Dorsey <bdo528@gmail.com>
2018-10-21 16:32:04 -0400

    logout functionality

--- app/config/passport.js
+++ app/config/passport.js
@@ -17,7 +17,7 @@ module.exports = function(passport){
       if (err){
         return done(err, false);
       }
-      console.log(opt.token);
+      console.log(opts.token);
       if (user){
           done(null, user);
       } else {
