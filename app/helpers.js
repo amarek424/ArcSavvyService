080419e55f7d6a4fae90936d65fdaecb3cddbe7d
amarek424 <amarek424@gmail.com>
2018-10-28 12:52:44 -0400

    attempt 2

--- app/helpers.js
+++ app/helpers.js
@@ -1,21 +1,21 @@
 const cryptoRandomString = require('crypto-random-string');
 
-export class helpers() {
-	// Generates a random hash and makes sure it doesn't match any of the existing
-	generateWhitehash(user) {
-		var hash = cryptoRandomString(8);
-		return hash;
-	}
 
-	// Removes a hash from the whitelist
-	removeFromWhitelist(user, hash) {
-		var tokenWhitelist = user.tokenWhitelist;
-		for (var i = tokenWhitelist.length-1; i >= 0; i--) {
-			if (tokenWhitelist[i] === hash) {
-				tokenWhitelist.splice(i, 1);
-				break;
-			}
-		}
-		return tokenWhitelist;
-	}
+// Generates a random hash and makes sure it doesn't match any of the existing
+exports.generateWhitehash(user) {
+	var hash = cryptoRandomString(8);
+	return hash;
 }
+
+// Removes a hash from the whitelist
+exports.removeFromWhitelist(user, hash) {
+	var tokenWhitelist = user.tokenWhitelist;
+	for (var i = tokenWhitelist.length-1; i >= 0; i--) {
+		if (tokenWhitelist[i] === hash) {
+			tokenWhitelist.splice(i, 1);
+			break;
+		}
+	}
+	return tokenWhitelist;
+}
+
