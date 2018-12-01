2fc14d879c113e4d92bd944407a9168740f2e4ae
amarek424 <amarek424@gmail.com>
2018-12-01 11:46:26 -0500

    jebus take the wheel

--- app/controllers/authorization.js
+++ app/controllers/authorization.js
@@ -92,7 +92,7 @@ exports.authenticateUser = (req, res) => {
               res.json({ success: false, message: 'Authentication failed. server error.'});
             } else {
               console.log(foundUser.verify);
-              if (foundUser.verify != null) {
+              if (foundUser.verify) {
                 res.json({ success: false, message: 'Email verification still required.', code: 6});
               } else {
                 foundUser.password = null;
@@ -156,25 +156,25 @@ exports.createNewValidateCode = (req, res) => {
 exports.verifyUser = (req, res) => {
   // find user and clear its unverified status
   user.findOneAndUpdate({
-    email: req.body.email,
-    verify: { $exists: true }
+    email: req.body.email
   },
   {
     $inc: { 'verify.attempts': -1 }
   }, function(err, foundUser){
     // if error or the user cannot be found, return error
     if (err){
-      return res.json({ success: false, message: 'Verification error'});
+      return res.json({ success: false, message: 'Verification error.'});
     } else if (foundUser == null) {
-      return res.json({ success: false, message: 'This user cannot be verified'});
-    } else if (foundUser.code != 0) {
-      if (foundUser.verify.attempts > 0) {
+      return res.json({ success: false, message: 'This user cannot be found.'});
+    } else if (foundUser.verify) {
+      return res.json({ success: false, message: 'This user is already verified.'})
+    } else if (foundUser.verify.attempts > 0) {
         if (foundUser.verify.code == req.body.code) {
           user.findOneAndUpdate({
             email: req.body.email
           },
           {
-            $unset: { verify: undefined } // Was 'null' instead of 'true'
+            $unset: { verify: undefined, verify.code: undefined, verify.attempts: undefined } // Was 'null' instead of 'true'
           },
           {
             multi: true, safe: true
